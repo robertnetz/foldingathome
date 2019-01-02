a70ceb4e59cb2422416db041cda8fe5d30a4b6c2
Shihta Kuan <shihta.kuan@gmail.com>
2019-01-02 09:39:07 +0800

    Add new environment: CPUS

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+CPUS=${CPUS:-'-1'}
+
 # recreate the template to be able to set properties
 cp /etc/fahclient/config_template.xml /etc/fahclient/config.xml
 echo "running fahclient as ${USERNAME} for the TEAM ${TEAM} and ${POWER} power"
-sed -i -e "s/{{USERNAME}}/$USERNAME/;s/{{TEAM}}/$TEAM/;s/{{POWER}}/$POWER/;s/{{PASSKEY}}/$PASSKEY/" /etc/fahclient/config.xml
+sed -i -e "s/{{USERNAME}}/$USERNAME/;s/{{TEAM}}/$TEAM/;s/{{POWER}}/$POWER/;s/{{PASSKEY}}/$PASSKEY/;s/{{CPUS}}/$CPUS/" /etc/fahclient/config.xml
 
 nohup /etc/init.d/FAHClient start >> /var/lib/fahclient/log.txt 2>&1 &
 tail -F /var/lib/fahclient/log.txt
